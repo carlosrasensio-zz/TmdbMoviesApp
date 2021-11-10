05a506b65f89d7b77ca96c87f8b0edea4dcd7699
Carlos Rodriguez Asensio <crodrigueza@vectoritcgroup.com>
2021-11-10 17:22:19 +0100

    Added RxSwift and RxCocoa pods to the project.

---
+++ TmdbMoviesApp/Pods/Target Support Files/Pods-TmdbMoviesApp-TmdbMoviesAppUITests/Pods-TmdbMoviesApp-TmdbMoviesAppUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TmdbMoviesApp_TmdbMoviesAppUITests : NSObject
+@end
+@implementation PodsDummy_Pods_TmdbMoviesApp_TmdbMoviesAppUITests
+@end
