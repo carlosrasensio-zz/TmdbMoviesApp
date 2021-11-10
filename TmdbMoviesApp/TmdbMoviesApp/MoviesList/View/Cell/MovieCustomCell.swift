e0bf745ec51fb9cdbe7c754be3f709a18604866f
Carlos Rodriguez Asensio <crodrigueza@vectoritcgroup.com>
2021-11-10 17:08:53 +0100

    Added xib and class for custom cell. Pending to configure.

---
+++ TmdbMoviesApp/TmdbMoviesApp/MoviesList/View/Cell/MovieCustomCell.swift
@@ -0,0 +1,23 @@
+//
+//  MovieCustomCell.swift
+//  TmdbMoviesApp
+//
+//  Created by crodrigueza on 9/11/21.
+//
+
+import UIKit
+
+class MovieCustomCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
