3cb768531a1a53f6c3d23f348d0739c4578b35b6
Carlos Rodriguez Asensio <crodrigueza@vectoritcgroup.com>
2021-11-11 02:35:37 +0100

    Reorganise constants file.

--- TmdbMoviesApp/TmdbMoviesApp/Utils/Constants.swift
+++ TmdbMoviesApp/TmdbMoviesApp/Utils/Constants.swift
@@ -8,24 +8,31 @@
 import Foundation
 
 struct Constants {
-    static let version = 3
-    static let apiKey = "5d8bb741e4498fd5448bc0738a12eb52"
+    struct NetworkManager {
+        static let version = 3
+        static let apiKey = "5d8bb741e4498fd5448bc0738a12eb52"
 
-    struct URL {
-        static let base = "https://api.themoviedb.org/\(Constants.version)/"
-        static let image = "hhtps://image.tmdb.org/t/p/w200"
+        struct URLs {
+            static let base = "https://api.themoviedb.org/\(Constants.NetworkManager.version)/"
+            static let image = "hhtps://image.tmdb.org/t/p/w200"
+        }
+
+        struct Endpoints {
+            static let apiKey = "?api_key=\(Constants.NetworkManager.apiKey)"
+            static let popularMovies = "movie/popular"
+        }
+
+        struct Optionals {
+            static let language = "&language="
+            static let numberOfPages = 1
+            static let page = "&page=\(numberOfPages)"
+            static let isIncludedAdult = false
+            static let adult = "&include_adult=\(isIncludedAdult)"
+        }
     }
 
-    struct Endpoint {
-        static let apiKey = "?api_key=\(Constants.apiKey)"
-        static let searchMovies = "search/movie"
+    struct CustomCells {
+        static let movieCellId = "MovieCustomCell"
     }
 
-    struct Optional {
-        static let language = "&language="
-        static let numberOfPages = 1
-        static let page = "&page=\(numberOfPages)"
-        static let isIncludedAdult = false
-        static let adult = "&include_adult=\(isIncludedAdult)"
-    }
 }
