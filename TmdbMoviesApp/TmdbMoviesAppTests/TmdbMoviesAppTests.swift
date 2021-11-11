1521617160e783c0ab15caaba404192b6a1b2c49
Carlos Rodriguez Asensio <crodrigueza@vectoritcgroup.com>
2021-11-11 14:25:23 +0100

    Added very simple test to check is the service is returning correct data.

--- TmdbMoviesApp/TmdbMoviesAppTests/TmdbMoviesAppTests.swift
+++ TmdbMoviesApp/TmdbMoviesAppTests/TmdbMoviesAppTests.swift
@@ -9,7 +9,10 @@ import XCTest
 @testable import TmdbMoviesApp
 
 class TmdbMoviesAppTests: XCTestCase {
+    // MARK: - Variables
+    let networkManager = NetworkManager()
 
+    // MARK: - Default UnitTesting methods
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
@@ -18,16 +21,10 @@ class TmdbMoviesAppTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    // MARK: - NetworkManager test
+    func testNetworkManager() {
+        let movies = networkManager.getMovies()
+        XCTAssertNotNil(movies)
     }
 
 }
