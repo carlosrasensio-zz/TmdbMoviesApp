baba9740c345a3ba0f919938bd23fe1da330429d
Carlos Rodriguez Asensio <crodrigueza@vectoritcgroup.com>
2021-11-11 02:39:13 +0100

    Modified table and cell configuration. Processed data from service to view, through view model. Using RxSwift for reactive programming.

--- TmdbMoviesApp/TmdbMoviesApp/MoviesList/View/MoviesListView.swift
+++ TmdbMoviesApp/TmdbMoviesApp/MoviesList/View/MoviesListView.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import RxSwift
 
 class MoviesListView: UIViewController {
 
@@ -16,26 +17,64 @@ class MoviesListView: UIViewController {
     // MARK: - Variables/Constants
     private var router = MoviesListRouter()
     private var viewModel = MoviesListViewModel()
+    private var disposeBag = DisposeBag()
+    private var movies = [Movie]()
 
     // MARK: - Life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
 
         viewModel.bind(view: self, router: router)
+        configureTableView()
+        getMovies()
+    }
+
+    // MARK: - Get data from ViewModel with RxSwift
+    private func getMovies() {
+        return viewModel.getMovies()
+            .subscribe(on: MainScheduler.instance)
+            .observe(on: MainScheduler.instance)
+            .subscribe { movies in
+                self.movies = movies
+                self.reloadTableView()
+            } onError: { error in
+                print("\n[X] Error: \(error.localizedDescription)\n")
+            } onCompleted: {
+            }.disposed(by: disposeBag)
+    }
+
+    // MARK: - TableView configuration
+    private func reloadTableView() {
+        DispatchQueue.main.async {
+            self.activityIndicator.stopAnimating()
+            self.activityIndicator.isHidden = true
+            self.tableView.reloadData()
+        }
+    }
+
+    private func configureTableView() {
+        tableView.rowHeight = UITableView.automaticDimension
+        tableView.register(UINib(nibName: Constants.CustomCells.movieCellId, bundle: nil), forCellReuseIdentifier: Constants.CustomCells.movieCellId)
     }
 }
 
 // MARK: - TableView functions
 extension MoviesListView: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return movies.count
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")!
-        cell.backgroundColor = .red
+        let cell = tableView.dequeueReusableCell(withIdentifier: Constants.CustomCells.movieCellId) as! MovieCustomCell
+        cell.titleLabel.text = movies[indexPath.row].title
+        cell.descriptionLabel.text = movies[indexPath.row].description
+        cell.voteAverageLabel.text = "Vote average: \(movies[indexPath.row].voteAverage)"
 
         return cell
+    }
+
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 200
     }
 }
 
--- TmdbMoviesApp/TmdbMoviesApp/MoviesList/ViewModel/MoviesListViewModel.swift
+++ TmdbMoviesApp/TmdbMoviesApp/MoviesList/ViewModel/MoviesListViewModel.swift
@@ -6,16 +6,23 @@
 //
 
 import Foundation
+import RxSwift
 
 class MoviesListViewModel {
     // MARK: - Variables/Constants
     private weak var view: MoviesListView?
     private var router: MoviesListRouter?
+    private var networkManager = NetworkManager()
 
     // MARK: - Connecting view and router
     func bind(view: MoviesListView, router: MoviesListRouter) {
         self.view = view
         self.router = router
         self.router?.setSourceView(view)
+    }
+
+    // MARK: - Get data from service
+    func getMovies() -> Observable<[Movie]> {
+        return networkManager.getMovies()
     }
 }
